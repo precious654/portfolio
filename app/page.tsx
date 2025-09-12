03d12f324ed4572c408cc5a9fd8828e0fa936730
precious654 <odukwechiemeka@gmail.com>
2025-09-12 16:16:53 +0100

    worked on navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,131 @@
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen w-9/12 mx-auto flex flex-col gap-10">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non
+        doloremque labore veniam commodi quae cum omnis ad cupiditate, dolorem
+        maxime amet vel porro et corporis? Dignissimos, natus ad suscipit quo
+        corporis laudantium numquam, tenetur sapiente quis omnis, similique hic
+        aspernatur reprehenderit ullam sit ex? Possimus ipsam corporis odit,
+        impedit quasi provident exercitationem sit architecto at perferendis ad
+        illo quaerat quia repudiandae magnam a non tenetur accusamus expedita,
+        rem, tempore eum quae suscipit. Veritatis consequatur nisi nemo! Nulla,
+        mollitia magni quisquam repellat hic nihil ut laudantium unde deleniti
+        amet voluptas excepturi dicta consequatur! Sunt velit perferendis ipsam
+        fugit odit voluptatum?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non
+        doloremque labore veniam commodi quae cum omnis ad cupiditate, dolorem
+        maxime amet vel porro et corporis? Dignissimos, natus ad suscipit quo
+        corporis laudantium numquam, tenetur sapiente quis omnis, similique hic
+        aspernatur reprehenderit ullam sit ex? Possimus ipsam corporis odit,
+        impedit quasi provident exercitationem sit architecto at perferendis ad
+        illo quaerat quia repudiandae magnam a non tenetur accusamus expedita,
+        rem, tempore eum quae suscipit. Veritatis consequatur nisi nemo! Nulla,
+        mollitia magni quisquam repellat hic nihil ut laudantium unde deleniti
+        amet voluptas excepturi dicta consequatur! Sunt velit perferendis ipsam
+        fugit odit voluptatum?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non
+        doloremque labore veniam commodi quae cum omnis ad cupiditate, dolorem
+        maxime amet vel porro et corporis? Dignissimos, natus ad suscipit quo
+        corporis laudantium numquam, tenetur sapiente quis omnis, similique hic
+        aspernatur reprehenderit ullam sit ex? Possimus ipsam corporis odit,
+        impedit quasi provident exercitationem sit architecto at perferendis ad
+        illo quaerat quia repudiandae magnam a non tenetur accusamus expedita,
+        rem, tempore eum quae suscipit. Veritatis consequatur nisi nemo! Nulla,
+        mollitia magni quisquam repellat hic nihil ut laudantium unde deleniti
+        amet voluptas excepturi dicta consequatur! Sunt velit perferendis ipsam
+        fugit odit voluptatum?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non
+        doloremque labore veniam commodi quae cum omnis ad cupiditate, dolorem
+        maxime amet vel porro et corporis? Dignissimos, natus ad suscipit quo
+        corporis laudantium numquam, tenetur sapiente quis omnis, similique hic
+        aspernatur reprehenderit ullam sit ex? Possimus ipsam corporis odit,
+        impedit quasi provident exercitationem sit architecto at perferendis ad
+        illo quaerat quia repudiandae magnam a non tenetur accusamus expedita,
+        rem, tempore eum quae suscipit. Veritatis consequatur nisi nemo! Nulla,
+        mollitia magni quisquam repellat hic nihil ut laudantium unde deleniti
+        amet voluptas excepturi dicta consequatur! Sunt velit perferendis ipsam
+        fugit odit voluptatum?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non
+        doloremque labore veniam commodi quae cum omnis ad cupiditate, dolorem
+        maxime amet vel porro et corporis? Dignissimos, natus ad suscipit quo
+        corporis laudantium numquam, tenetur sapiente quis omnis, similique hic
+        aspernatur reprehenderit ullam sit ex? Possimus ipsam corporis odit,
+        impedit quasi provident exercitationem sit architecto at perferendis ad
+        illo quaerat quia repudiandae magnam a non tenetur accusamus expedita,
+        rem, tempore eum quae suscipit. Veritatis consequatur nisi nemo! Nulla,
+        mollitia magni quisquam repellat hic nihil ut laudantium unde deleniti
+        amet voluptas excepturi dicta consequatur! Sunt velit perferendis ipsam
+        fugit odit voluptatum?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non
+        doloremque labore veniam commodi quae cum omnis ad cupiditate, dolorem
+        maxime amet vel porro et corporis? Dignissimos, natus ad suscipit quo
+        corporis laudantium numquam, tenetur sapiente quis omnis, similique hic
+        aspernatur reprehenderit ullam sit ex? Possimus ipsam corporis odit,
+        impedit quasi provident exercitationem sit architecto at perferendis ad
+        illo quaerat quia repudiandae magnam a non tenetur accusamus expedita,
+        rem, tempore eum quae suscipit. Veritatis consequatur nisi nemo! Nulla,
+        mollitia magni quisquam repellat hic nihil ut laudantium unde deleniti
+        amet voluptas excepturi dicta consequatur! Sunt velit perferendis ipsam
+        fugit odit voluptatum?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non
+        doloremque labore veniam commodi quae cum omnis ad cupiditate, dolorem
+        maxime amet vel porro et corporis? Dignissimos, natus ad suscipit quo
+        corporis laudantium numquam, tenetur sapiente quis omnis, similique hic
+        aspernatur reprehenderit ullam sit ex? Possimus ipsam corporis odit,
+        impedit quasi provident exercitationem sit architecto at perferendis ad
+        illo quaerat quia repudiandae magnam a non tenetur accusamus expedita,
+        rem, tempore eum quae suscipit. Veritatis consequatur nisi nemo! Nulla,
+        mollitia magni quisquam repellat hic nihil ut laudantium unde deleniti
+        amet voluptas excepturi dicta consequatur! Sunt velit perferendis ipsam
+        fugit odit voluptatum?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non
+        doloremque labore veniam commodi quae cum omnis ad cupiditate, dolorem
+        maxime amet vel porro et corporis? Dignissimos, natus ad suscipit quo
+        corporis laudantium numquam, tenetur sapiente quis omnis, similique hic
+        aspernatur reprehenderit ullam sit ex? Possimus ipsam corporis odit,
+        impedit quasi provident exercitationem sit architecto at perferendis ad
+        illo quaerat quia repudiandae magnam a non tenetur accusamus expedita,
+        rem, tempore eum quae suscipit. Veritatis consequatur nisi nemo! Nulla,
+        mollitia magni quisquam repellat hic nihil ut laudantium unde deleniti
+        amet voluptas excepturi dicta consequatur! Sunt velit perferendis ipsam
+        fugit odit voluptatum?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero non
+        doloremque labore veniam commodi quae cum omnis ad cupiditate, dolorem
+        maxime amet vel porro et corporis? Dignissimos, natus ad suscipit quo
+        corporis laudantium numquam, tenetur sapiente quis omnis, similique hic
+        aspernatur reprehenderit ullam sit ex? Possimus ipsam corporis odit,
+        impedit quasi provident exercitationem sit architecto at perferendis ad
+        illo quaerat quia repudiandae magnam a non tenetur accusamus expedita,
+        rem, tempore eum quae suscipit. Veritatis consequatur nisi nemo! Nulla,
+        mollitia magni quisquam repellat hic nihil ut laudantium unde deleniti
+        amet voluptas excepturi dicta consequatur! Sunt velit perferendis ipsam
+        fugit odit voluptatum?
+      </p>
     </div>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Navbar = () => {
+  const navRef = React.useRef<HTMLElement | null>(null);
+
+  useGSAP(
+    () => {
+      // Create timeline for navbar animation
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: "body", // Use body as trigger for full page scroll
+          start: "top top", // Start immediately when scrolling begins
+          end: "50% top", // End when middle of page reaches top of viewport
+          scrub: 0.1, // Smooth scrubbing with 1 second lag
+          markers: false, // Set to true for debugging
+          invalidateOnRefresh: true,
+        }
+      });
+
+      // Animate navbar out
+      timeline.to(navRef.current, {
+        y: -100, // Move up and out of view
+        opacity: 0, // Fade out
+        ease: "power1.inOut"
+      });
+      setTimeout(() => ScrollTrigger.refresh(), 100);
+    },
+    { scope: navRef }
+  );
+
+  return (
+    <header 
+      ref={navRef}
+      className="w-full py-10 flex justify-center items-center fixed top-0 left-0 z-50"
+    >
+      <nav className="flex items-center justify-between lg:w-5/12 p-4 rounded-lg bg-background/20 backdrop-blur-lg">
+        <Link href="/">
+          Home
+        </Link>
+        <Link href="/">
+          About Me
+        </Link>
+        <Link href="/">
+          Projects
+        </Link>
+        <Link href="/">
+          Blog
+        </Link>
+		<div className="w-0.5 h-5 bg-gray-300"></div>
+		<button>Menu</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
